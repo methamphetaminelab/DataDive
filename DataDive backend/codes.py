e4497cdd63245e9765d8af2f55645a13cba9c6d0
methamphetaminelab <58376312+methamphetaminelab@users.noreply.github.com>
2024-10-12 23:14:13 +0300

    Added functionality to load and sort data from various file formats (xlsx, csv, text)

---
+++ DataDive backend/codes.py
@@ -0,0 +1,19 @@
+RESPONSE_CODES = {
+    0: "success",
+    1: "ERROR wrong file type",
+    2: "ERROR wrong file path",
+    3: "ERROR wrong mode",
+    4: "ERROR wrong display type",
+    5: "ERROR wrong language",
+    6: "ERROR wrong columns",
+    7: "ERROR wrong sort mode",
+    8: "ERROR wrong sort method",
+    99: "ERROR file not found",
+    101: "ERROR unknown error or exception"
+}
+
+MODES = {
+    1: "xlsx",
+    2: "csv",
+    3: "text"
+}
---
+++ DataDive backend/sortData.py
@@ -0,0 +1,24 @@
+from codes import RESPONSE_CODES
+
+def sortData(data):
+    mode = int(input('Choose sort type "by column/by row" or "by row/by column" (1,2): '))
+    method = int(input('Choose sort order "increasing/decreasing" (1,2): '))
+
+    if mode == 1:
+        if method == 1:
+            results = data.sort_values(by=data.columns[0], ascending=True)
+        elif method == 2:
+            results = data.sort_values(by=data.columns[0], ascending=False)
+        else:
+            raise ValueError(RESPONSE_CODES[8])
+    elif mode == 2:
+        if method == 1:
+            results = data.sort_values(by=data.index, ascending=True)
+        elif method == 2:
+            results = data.sort_values(by=data.index, ascending=False)
+        else:
+            raise ValueError(RESPONSE_CODES[8])
+    else:
+        raise ValueError(RESPONSE_CODES[7])
+    
+    return results
